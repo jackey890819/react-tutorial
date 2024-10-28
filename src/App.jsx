03df83558812c28a22c27324d231167e5f57a025
Shun-Fa Yang <jackey890819@gmail.com>
2024-10-28 22:12:40 +0800

    feat: handle window resize event

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import LoginForm from "./components/LoginForm";
 export default function App() {
+  window.addEventListener('resize', (event) => {
+    console.log(`window's width: ${window.innerWidth}px.`);
+    console.log(`window's height: ${window.innerHeight}px.`);
+  })
   return (
     <div>
       <LoginForm />
